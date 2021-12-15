7351231f9b80b3bc6f151dcee82c6cde9b94f517
ishmalkhalid <ishmal2000@gmail.com>
2021-12-14 21:45:19 -0500

    comments

--- sketch.js
+++ sketch.js
@@ -1,34 +1,36 @@
-
+// global variables
 let carX, carZ;
 let car;
 let moveSpeed;
 let world;
+
+// truck, overhead fuelbar and the number of coins
 let truck, fuelbox, cointext;
 
+// trees and bricks arrays
 let trees = [];
 let boxes = [];
+
+// variables to create entities
 let cx, cy, cz, total;
 
+// bowling pins array
 let pins = [];
 
-let pump;
+// road block
+let rblock;
 
-let field;
-
+// streetlights
 let lights = [];
 
-let road1, road2, road3;
-
-let reset = false;
-
+// bowling
 let bowling;
 
-let fuel;
-
+// rain and coins array
 let particles = [];
-
 let coins = [];
 
+// number of coins collected
 let collections = 0;
 
 // user position & velocity
@@ -92,6 +94,7 @@ function setup() {
 		trees.push(new Tree(random(-50, -10), random(-40, 50), random(2.5, 3.3)));
 	}
 
+	// call entity placement functions
 	placebowling();
 	placefuel();
 	placebrick();
@@ -109,14 +112,16 @@ function setup() {
 }
 
 function draw() {
+	sounds();
 
 	world.camera.holder.object3D.rotation.set(radians(-25), 0, 0);
+	
 	// always create a new rain particle
 	var temp = new Particle(random(-20, 20), 15, random(-80,10));
 
 	particles.push( temp );
 
-	// draw all rain
+	// draw all rain and delete if reaches the ground
 	for (var i = 0; i < particles.length; i++) {
 		var result = particles[i].move();
 		if (result == "gone") {
@@ -124,15 +129,16 @@ function draw() {
 			i-=1;
 		}
 	}
-	// console.log(fuelbox.getWidth())
+
+	// decrement fuel after some time
 	if (frameCount % 200 == 0 && fuelbox.getWidth() >= 0.09){
 		fuelbox.setWidth(fuelbox.getWidth()-0.09);
 	}
+
+	// only move car if there is fuel
 	if (fuelbox.getWidth() > 0.01) {
 		carMovement();
 	}
-
-	// console.log(truck.x, truck.z)
 
 	// add coins
 	if (frameCount % 100 == 0){
@@ -146,10 +152,12 @@ function draw() {
 			i-=1;
 		}
 	}
+	// update number of coins on the car
 	cointext.setText(collections)
 
 }
 
+// place the fences around the playground
 function placefence (){
 	// gate Model
 	let gate = new GLTF({
@@ -165,6 +173,7 @@ function placefence (){
 	let cx = -25.6;
 	let bx = 17;
 	let fence;
+	// add fences on the right and left of teh gate
 	for (let i = 0; i < 4; i++){
 		fence = new GLTF({
 			asset: 'fence',
@@ -172,7 +181,7 @@ function placefence (){
 			scaleX:2,scaleY:2,scaleZ:2,
 			rotationY:68
 		});
-    fence.spinY(-5);
+    	fence.spinY(-5);
 		fence.tag.setAttribute('dynamic-Body', "shape: box; halfExtents: 0.05 0.05 0.02; mass: 0.5");
 		world.add(fence);
 		cx -= 8.3;
@@ -186,6 +195,7 @@ function placefence (){
 		world.add(fence);
 		bx += 8.3;
 	}
+	// add fences on the left and right of the playground
 	cz = - 95
 	for (let i = 0; i < 5; i++){
 		fence = new GLTF({
@@ -207,6 +217,7 @@ function placefence (){
 		cz += 8.3;
 	}
 
+	// add fences behind the playground
 	cx = -50;
 	for (let i = 0; i < 12; i++){
 		fence = new GLTF({
@@ -221,6 +232,7 @@ function placefence (){
 	}
 }
 
+// create the playground
 function playground() {
 	// create a plane to serve as our "ground"
 	let g = new Plane({
@@ -233,9 +245,11 @@ function playground() {
 	world.add(g);
 }
 
+// place the roads
 function placeroad (){
+	// connect two roads infront of each other
 	// Road Model
-	road1 = new GLTF({
+	let road1 = new GLTF({
 		asset: 'road',
 		x: 0,
 		y: 0,
@@ -246,7 +260,7 @@ function placeroad (){
 		// rotationX:radians(80)
 	});
 	// Road Model
-	road2 = new GLTF({
+	let road2 = new GLTF({
 		asset: 'road',
 		x: 0,
 		y: 0,
@@ -261,8 +275,10 @@ function placeroad (){
 	world.add(road2);
 }
 
+// place the roadblock as well as playing instructions behind the car
 function placeRoadBlocks(){
-	let rblock = new GLTF({
+	// load new roadblock
+	rblock = new GLTF({
 		asset: 'rblock',
 		x: 0,y: 0, z:-15,
 		scaleX:0.03, scaleY:0.03,scaleZ:0.03,
@@ -271,7 +287,7 @@ function placeRoadBlocks(){
 	rblock.tag.setAttribute('dynamic-Body', "linearDamping: 0.5; mass: 10");
 	world.add(rblock)
 
-	// fuel text
+	// WASD instructions text
 	let movetext = new Text({
 		text: 'WASD to move',
 		red: 51, green: 51, blue: 204,
@@ -283,7 +299,7 @@ function placeRoadBlocks(){
 
 	world.add(movetext);
 
-	// fuel text
+	// honk text
 	let honktext = new Text({
 		text: 'H to honk',
 		red: 51, green: 51, blue: 204,
@@ -296,9 +312,9 @@ function placeRoadBlocks(){
 	world.add(honktext);
 }
 
+// controls how the car moves, its acceleration and camera movemnet alonsgide it
 function carMovement () {
-	// if (moveX == 0)
-
+	// if  S is pressed, play engine sound and set velocity
 	if (keyIsDown(65)) {
 		if(!carDrive.isPlaying()){
 			carDrive.play()
@@ -306,6 +322,7 @@ function carMovement () {
 		//world.camera.nudgePosition(-moveSpeed, 0, 0);
 		velocity.add(-accel, 0);
 	}
+	// if W is pressed
 	if (keyIsDown(68)) {
 		if(!carDrive.isPlaying()){
 			carDrive.play()
@@ -313,7 +330,7 @@ function carMovement () {
 		//world.camera.nudgePosition(moveSpeed, 0, 0);
 		velocity.add(accel, 0);
 	}
-
+	// if D is pressed
 	if (keyIsDown(87)) {
 		if(!carDrive.isPlaying()){
 			carDrive.play()
@@ -321,6 +338,7 @@ function carMovement () {
 		//world.camera.nudgePosition(0, 0, -moveSpeed);
 		velocity.add(0, -accel);
 	}
+	// if A is pressed
 	if (keyIsDown(83)) {
 		if(!carDrive.isPlaying()){
 			carDrive.play()
@@ -412,6 +430,14 @@ function carMovement () {
 	}
 }
 
+// add sounds
+function sounds(){
+	if (dist(truck.x, truck.z, rblock.x, rblock.z) <= 3){
+		console.log("touchhh")
+	}
+}
+
+// place the truck
 function placecar() {
 	carX = 0;
 	carZ = 0;
@@ -454,10 +480,12 @@ function placecar() {
 	world.add(truck);
 }
 
+//  place street lights
 function placelamps() {
 	// Street Light Model
 	cz = 20
 	let light;
+	// place street lights on the left and the right
 	for (let i = 0; i < 8; i++){
 		light = new GLTF({
 			asset: 'light',
@@ -486,8 +514,9 @@ function placelamps() {
 	}
 }
 
+// place bricks in the playground
 function placebrick () {
-	// add bricks
+	// add bricks that are of two layers
 	for (let j = 0; j < 10; j++ ){
 		total = random(1, 7)
 		cx = random(-30, 30);
@@ -506,6 +535,7 @@ function placebrick () {
 
 	cy = 0.25;
 	cz = -80;
+	// add the taller brick towers
 	for (let i = 0; i < 7; i++) {
 		cx = 15;
 		for (let i = 0; i < 5; i++) {
@@ -518,6 +548,7 @@ function placebrick () {
 
 	cy = 0.25;
 	cx = -15;
+	// add the second taller brick tower in different direction
 	for (let i = 0; i < 6; i++) {
 		cz = -70;
 		for (let i = 0; i < 6; i++) {
@@ -529,6 +560,7 @@ function placebrick () {
 	}
 }
 
+// position bowling pins and bowling ball
 function placebowling () {
 	// add bowling pins
 	let cx = -22
@@ -556,15 +588,16 @@ function placebowling () {
 		// rotationY:30
 	});
 
-  ball.tag.setAttribute('body', "type: dynamic; mass: 50; shape: none;");
-  ball.tag.setAttribute('shape__main', "shape: sphere; radius:1.4;");
+ 	ball.tag.setAttribute('body', "type: dynamic; mass: 50; shape: none;");
+  	ball.tag.setAttribute('shape__main', "shape: sphere; radius:1.4;");
 
 	world.add(ball);
 }
 
+// place fuel pump and button on it
 function placefuel () {
 	// Pump Model
-	pump = new GLTF({
+	let pump = new GLTF({
 		asset: 'pump',
 		x: 6,
 		y: 0.5,
@@ -589,14 +622,16 @@ function placefuel () {
 		radius: 0.2,
 		red: 0, green:255, blue:0,
 		rotationX: 90,
+		// hover on the button to change its color to indicate that it can be clicked
 		enterFunction: function(theBox) {
-			// make the cube change color
+			// make the button change color
 			theBox.setColor(0, 153, 51);
 		},
 		leaveFunction: function(theBox) {
-			// make the cube original color
+			// make the button original color
 			theBox.setColor(0, 255, 0);
 		},
+		// click on button to fill fuel tank
 		clickFunction:  function(entity) {
 			console.log(truck.x, truck.z)
 			if (truck.x >= 3 &&
@@ -628,23 +663,9 @@ function placefuel () {
 
 }
 
-function keyPressed(){
-	if (keyCode == 66){
-		reset = true
-		// for (let i = 0; i < boxes.length; i++){
-			// console.log("currX " + boxes[0].box.x)
-			boxes[0].box.x = boxes[0].px;
-			// console.log("newX " + boxes[0].box.x)
-			boxes[0].box.y = boxes[0].py;
-			// console.log("newy " + boxes[0].box.x)
-			boxes[0].box.z = boxes[0].pz;
-			// console.log("newz " + boxes[0].box.x)
-	}
-}
-
 // coin class
 class Coin {
-	// gate Model
+	// coin Model
 	constructor(){
 		this.coin = new GLTF({
 			asset: 'coin',
@@ -655,13 +676,15 @@ class Coin {
 		world.add(this.coin);
 		this.time = 0;
 	}
-
+	// destroy coins after some time or when the user picks them up
 	destroy(){
 		this.time += 1
+		// if they havent been picked up, remove after a certain time
 		if (this.time == 700){
 			world.remove(this.coin)
 			return "gone"
 		}
+		// if they are picked up, remove, play music and increase coin count
 		else if (dist(truck.x, truck.z, this.coin.x, this.coin.z) <= 2){
 			collections += 1
 			coinCol.play()
@@ -696,13 +719,14 @@ class Cube {
 		this.px = x;
 		this.py = y;
 		this.pz = z;
-
+		// make bricks dynamic
 		this.box.tag.setAttribute('dynamic-Body', "linearDamping: 0.5; mass: 10");
 		world.add(this.box);
 	}
 
 }
 
+// Bowling Pins class
 class BowlingPins {
 	constructor(x,z) {
 		// bowling pins Model
@@ -716,6 +740,7 @@ class BowlingPins {
 			scaleZ:0.0015
 			// rotationY:30
 		});
+		// make bowling pins dynamic
    		bowling.tag.setAttribute('dynamic-Body', "shape: cylinder; radiusTop: 0.05; radiusBottom: 0.05; cylinderAxis: y; mass: 25");
 		world.add(bowling);
 	}
@@ -740,7 +765,7 @@ class Tree {
 			radiusBottom: 1.4, radiusTop: 0.01,
 			red: random(20, 40), green:random(120, 140), blue:0
 		});
-    this.stem.tag.object3D.userData.solid = true;
+    	this.stem.tag.object3D.userData.solid = true;
 
 		// add stem and leaves to container
 		this.stem.tag.setAttribute('static-Body', true);
@@ -807,9 +832,11 @@ class Particle {
 
 // play sound
 function keyPressed(){
+	// play rain sound in background
 	if(!rain.isPlaying()){
 		rain.loop();
 	}
+	// play horn when H is pressed
 	if (keyCode == 72){
 		honk.play()
 	}
